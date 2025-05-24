61b560c378a084cc22991fbc65013be4fce0eab2
Sumanth Bojugu <bojugusumanthkumar@gmail.com>
2025-05-24 14:59:16 +0530

    added sql db logic for simple get and post

--- main.go
+++ main.go
@@ -4,22 +4,30 @@ import (
 	"net/http"
 	"time"
 
+	"example.com/go-basic-backend/db"
 	"example.com/go-basic-backend/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
+
 	server := gin.Default()
 
 	server.GET("/events", func(ctx *gin.Context) {
-		events := models.GetAllEvents()
+		events, err := models.GetAllEvents()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch rows from db", "error": err})
+			return
+		}
+
 		ctx.JSON(http.StatusOK, gin.H{"message": "successful", "events": events})
 	})
 
 	server.POST("/events", func(ctx *gin.Context) {
 		var event models.Event
-		err := ctx.ShouldBindJSON(&event)
 
+		err := ctx.ShouldBindJSON(&event)
 		if err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not parse the request", "error": err})
 			return
@@ -27,11 +35,20 @@ func main() {
 
 		event.ID = 1
 		event.UserID = 1
-		event.Date = time.Now()
+		event.DateTime = time.Now()
 
-		event.Save()
+		err = event.Save()
 
-		events := models.GetAllEvents()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not create the event", "error": err})
+			return
+		}
+
+		events, err := models.GetAllEvents()
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch rows from db", "error": err})
+			return
+		}
 
 		ctx.JSON(http.StatusCreated, gin.H{"message": "event created!", "events": events})
 	})
--- models/event.go
+++ models/event.go
@@ -1,22 +1,83 @@
 package models
 
-import "time"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"time"
+
+	"example.com/go-basic-backend/db"
+)
 
 type Event struct {
-	ID          int
-	Title       string `binding:"required"`
+	ID          int64
+	Name        string `binding:"required"`
 	Description string `binding:"required"`
 	Location    string `binding:"required"`
-	Date        time.Time
+	DateTime    time.Time
 	UserID      int
 }
 
-var events = []Event{}
+func (e Event) Save() error {
+	query := `
+		INSERT INTO events(name, description, location, dateTime, user_id)
+		VALUES (?, ?, ?, ?, ?)
+	`
 
-func (e Event) Save() {
-	events = append(events, e)
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	e.DateTime = time.Now()
+	userID, err := rand.Int(rand.Reader, big.NewInt(10000))
+	if err != nil {
+		return err
+	}
+
+	e.UserID = int(userID.Int64()) + 1
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	e.ID = id
+
+	return nil
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	fmt.Println(events)
+
+	return events, nil
 }
